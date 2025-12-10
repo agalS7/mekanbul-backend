ddb47c2c54c32a68da2eb239761df31a0d573287
Ramazan <44579375+RaknaX@users.noreply.github.com>
2025-12-10 23:47:26 +0300

    Vercel Deployment Fix

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-require("./api/models/db");
+require("./app_api/models/db");
 
-const apiRouter = require("./api/routes/index");
+const apiRouter = require("./app_api/routes/index");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", apiRouter);
 
