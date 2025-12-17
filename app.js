41ff545d34dc382ad317417ae12f731a1469b6d1
Ramazan <44579375+RaknaX@users.noreply.github.com>
2025-12-17 14:08:45 +0300

    Allow Cross Domains

--- app.js
+++ app.js
@@ -16,4 +16,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", apiRouter);
 
+const allowCrossDomain = (_, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization",
+    );
+    next();
+};
+
+app.use(allowCrossDomain);
+
 module.exports = app;
